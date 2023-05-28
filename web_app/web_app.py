0c4057ecd93f45e74b20324e19453c86f856ea39
matt0164 <131754771+matt0164@users.noreply.github.com>
2023-05-28 17:05:55 -0400

    added

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -1,3 +1,5 @@
+import logging
+
 from dotenv import load_dotenv
 import os
 
@@ -10,11 +12,19 @@ load_dotenv(dotenv_path="/var/www/frizz/.env")
 
 app = Flask(__name__)
 
+# Enable debug mode temporarily for troubleshooting
+app.debug = False
+
+# Set up a logger
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='/var/www/frizz/logs/frizz.log', level=logging.DEBUG)
+
 def random_color():
     return f"#{random.randint(0, 0xFFFFFF):06x}"
 
 @app.route('/location', methods=['POST'])
 def location():
+    logger.debug('Received request for /location')
     data = request.get_json()
     lat = data['lat']
     lon = data['lng']
@@ -23,6 +33,7 @@ def location():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    logger.debug('Received request for index route')
     result = None
     photo = None
     color1 = random_color()
@@ -32,12 +43,11 @@ def index():
         weather_option = request.form.get("weather_option")
 
         if weather_option == "zip_code":
+            logger.debug('Weather option is zip_code')
             zip_code = request.form.get("zip_code")
             result, photo = get_weather_by_zip(zip_code)
         elif weather_option == "location":
+            logger.debug('Weather option is location')
             result, photo = get_weather_by_location()
 
     return render_template("frizz_landing.html?v=2", result=result, photo=photo, color1=color1, color2=color2)
-
-if __name__ == "__main__":
-    app.run(debug=True)
